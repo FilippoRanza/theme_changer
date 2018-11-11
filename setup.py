3acf69c4cd25b244d50b14834840b2f9cba5d65c
Filippo Ranza <filipporanza@gmail.com>
2018-11-11 23:11:21 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='theme_changer',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/FilippoRanza',
+    license='GNU General Public License v3.0',
+    author='filippo ',
+    author_email='',
+    description='automatically change wallpaper folder on KDE desktop',
+    scripts=['theme_changer.py']
+)
